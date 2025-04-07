de74ec44a83920a04b49a939d022dad19b2b21e5
AlexMeehan54 <ameehan366@wcupa.edu>
2025-04-07 13:41:23 -0400

    email update

---
+++ email/EmailStats.java
@@ -0,0 +1,20 @@
+package email;
+class EmailStats {
+
+    +toString(): String
+
+    int phrase_amount;
+    int word_amount;
+    int letter_amount;
+
+    public EmailStats(int phrase_amount, int word_amount, int letter_amount){
+        this.phrase_amount = phrase_amount;
+        this.word_amount = word_amount;
+        this.letter_amount = letter_amount;
+    }
+    
+    public String toString() {
+        return phrase_amount + word_amount + letter_amount;
+    }
+
+}
